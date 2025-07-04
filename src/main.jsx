0a5cc624359eb0971b8bb55f07eb46679d831fc5
Caleb Muturi <calebmuturi2024@gmail.com>
2025-07-04 15:30:16 +0300

    Improving Reusability with Props

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 // import './index.css'
 // import App from './App.jsx'
-import StarRating from './StarRating.jsx'
+import StarRating from './StarRating.jsx';
+import Test from './Test.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5}/>
-  </StrictMode>,
-)
+    <StarRating
+      maxRating={5}
+      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
+
+    <Test />
+  </StrictMode>
+);
